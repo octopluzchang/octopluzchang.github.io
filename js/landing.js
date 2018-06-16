834e8bb2eec08830a1eee62f47f08fb845760386
octopluzchang <ckenny0215@gmail.com>
2018-06-16 13:36:20 +0800

    new project update

--- js/landing.js
+++ js/landing.js
@@ -1,5 +1,13 @@
 $(document).ready(function() {
-            setTimeout(function() {
-                $('#textContent').addClass('show')
-            }, 1000);
+    setTimeout(function() {
+        $('.textContent').first().addClass('show')
+    }, 1000);
+    $('.next').click(function(){
+        $('.textContent').first().removeClass('show')
+        $('.textContent').last().addClass('show')
+    })
+    $('.back').click(function(){
+        $('.textContent').first().addClass('show')
+        $('.textContent').last().removeClass('show')
+    })
 })
